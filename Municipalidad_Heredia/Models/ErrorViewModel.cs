3604827e7bafc6b155f0985c5f6eb82e9676a29e
Wsanchez00244 <wsanchez00244@ufide.ac.cr>
2022-08-25 21:24:17 -0600

    Proyecto en blanco

---
+++ Municipalidad_Heredia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Municipalidad_Heredia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
